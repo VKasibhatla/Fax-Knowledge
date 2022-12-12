6a29621fdd6159a631a0221c08f4514571ecfa7f
Varun Kasibhatla <varunkasibhatla@varuns-air-2.devices.brown.edu>
2022-12-11 22:27:13 -0500

    Cleaned and left comments

--- preprocessing.py
+++ preprocessing.py
@@ -4,37 +4,33 @@ import random
 
 def fix(a):
     if a.startswith('"') and a.endswith('"'):
-        a = a[1:-1]
+        a = a[1:-1] #removes extraneous quotation marks
     if not a.endswith((".", "!", "?")):
-        a = a + "."
+        a = a + "." #adds periods to sentences without ending punctuation
     instances = re.compile(r"COVID-19", re.IGNORECASE)
-    a = instances.sub("coronavirus", a)
+    a = instances.sub("coronavirus", a) #changes instances of COVID-19 to coronavirus (see methods section)
     return a
 
 def parse():
-    token_dic = {}
     with open('data.txt') as f:
         lines = f.readlines()
-    random.shuffle(lines)
-    data = [[line.split(',',2)[1],fix(line.split(',',2)[2][:-3])] for line in lines]
+    random.shuffle(lines) #Shuffles data, which prevents the same set of statements from being in training and testing set
+    data = [[line.split(',',2)[1],fix(line.split(',',2)[2][:-3])] for line in lines] #implements the fix() method, as outlined above
     train_data = data[:int(0.75*len(data))]
     print(len(train_data))
     test_data = [x for x in data if x not in train_data]
     print(len(test_data))
     vocabulary, vocab_size = {}, 2 #initializing COVID-19 and UNK tokens
     vocabulary['covid-19'] = 0
-    vocabulary['coronavirus'] = 0
-    vocabulary['UNK'] = 1  
-    preset = ['covid-19','coronavirus','UNK']
+    vocabulary['coronavirus'] = 0 #additional check, covid-19 and coronavirus given the same token
+    vocabulary['UNK'] = 1 #UNK token used for words which appear in the testing set but do not appear in the training set
     claims = []
-    frequency = {}
-    UNK_list = [] 
     max = 0
     for x in range(0,len(test_data)):
         claim = test_data[x][1]
         tokens = claim.split()
         if len(tokens)>max:
-            max = len(tokens)
+            max = len(tokens) #gets max length sentence to pad other sentences to same length
     for x in range(0,len(train_data)):
         claim = train_data[x][1]
         claims.append(claim)
@@ -42,23 +38,13 @@ def parse():
         if len(tokens)>max:
             max = len(tokens)
         for token in tokens:
-            token = token.lower().replace('"', '').replace("'", '').replace('�?','')
-            if token not in vocabulary:
+            token = token.lower().replace('"', '').replace("'", '').replace('�?','') #additional cleaning procedures
+            if token not in vocabulary: #tokenizing input words for LSTM model
                 vocabulary[token] = vocab_size
                 vocab_size += 1
-                frequency[token] = 1
-                UNK_list.append(token)
-            else:
-                if token not in preset:
-                    frequency[token] = frequency[token] + 1
-                if token in UNK_list:
-                    UNK_list.remove(token)
-    print('GZmax',max)
     tokenize = lambda x: [vocabulary[key.lower().replace('"', '').replace("'", '').replace('�?','')] if key in vocabulary else vocabulary['UNK'] for key in x.split()]
     encode = lambda x: [1,0,0] if x == "F" else ([0,1,0] if x == "U" else [0,0,1])
     lstm_encode = lambda x: 0 if x == "F" else (1 if x == "U" else 2)
-    #lstm_encode = lambda x: random.choices([0, 1, 2])[0]
-    #lstm_encode = lambda x: random.choices([0, 1, 2], [0.1, 0.2, 0.7])[0]
     lstm_training = [[pad(tokenize(claims[x]),max,len(vocabulary)) for x in range(0,len(claims))],[lstm_encode(train_data[x][0]) for x in range(0,len(claims))]]
     test_claims = [test_data[x][1] for x in range(0,len(test_data))]
     lstm_testing = [[pad(tokenize(test_claims[x]),max,len(vocabulary)) for x in range(0,len(test_claims))],[lstm_encode(test_data[x][0]) for x in range(0,len(test_claims))]]
@@ -67,14 +53,10 @@ def parse():
     vocabulary['**PAD**'] = vocab_size
     vocabulary['**STOP**'] = vocab_size + 1
     vocab_size += 2
-    # for claim in claims:
-    #     token_dic[pad(tokenize(claims[x]),max,len(vocabulary))] = [claim,encode(train_data[x][0])]
+    return [fax_training,lstm_training],[fax_testing,lstm_testing], vocabulary
 
-    print('fax',lstm_testing)
-    return [fax_training,lstm_training],[fax_testing,lstm_testing], vocabulary, token_dic
-
-def pad(a,max,size):
+def pad(a,max,size): #pads all token sequences to be the same length using pad tokens followed by a final stop token
     for x in range(0,max-len(a)):
         a.append(size)
-    a.append(size+1)
+    a.append(size+1) 
     return a
