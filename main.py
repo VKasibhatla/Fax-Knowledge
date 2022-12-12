6c999fb5fa0cbd3af9d703119ed615360a282434
josue-creator <josue_cruz@brown.edu>
2022-12-11 22:32:55 -0500

    Cleaned up code

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ def train(model, train_input, train_labels, batch_size):
     total_seen = total_correct = 0
     for batch in range(batches):
         with tf.GradientTape() as tape:
-            inputs = train_input[batch*batch_size:(batch+1)*batch_size]
+            inputs = train_input[batch_size * batch : batch_size * (batch+1)]
         
             prbs = model.call(inputs)
-            labels = train_labels[batch*batch_size:(batch+1)*batch_size]
+            labels = train_labels[batch_size * batch : batch_size * (batch+1)]
             guesses =tf.math.argmax(prbs, axis=1).numpy() 
         
             loss = model.loss(prbs, labels)
@@ -75,42 +75,35 @@ if __name__ == "__main__":
     Read in COVID rumors dataset and initialize/train/test model.
     """ 
 
-    # demo_sentences = ["Australians trapped in Wuhan say they need to pay $673 to be rescued.", 
-    # "You will get a free coronavirus test by donating blood.",
-    # "The 2019 coronavirus causes sudden death syndrome.",
-    # "COVID-19 emerged in December 2019."]
+    demo_sentences = ["Australians trapped in Wuhan say they need to pay $673 to be rescued.", 
+    "You will get a free coronavirus test by donating blood.",
+    "The 2019 coronavirus causes sudden death syndrome.",
+    "COVID-19 emerged in December 2019."]
 
-    demo_sentences = []
     demo_labels = [1,0,0,2]
 
     training = False
-    paper_model = True
 
     ## Read in COVID rumors data,
     pre = preprocess.parse()
     
-    model = FaxModel(3) if paper_model else None
+    model = FaxModel(3) 
 
     if(training):
-        if(paper_model):
-            train_inputs = pre[0][0][0]
-            train_labels = pre[0][0][1]
-            test_inputs = pre[1][0][0]
-            test_labels = pre[1][0][1]
-            train(model, train_inputs, train_labels, 100)
-            
-            
-
-    if (len(demo_sentences) == 0):
+        train_inputs = pre[0][0][0]
+        train_labels = pre[0][0][1]
+        test_inputs = pre[1][0][0]
+        test_labels = pre[1][0][1]
+        train(model, train_inputs, train_labels, 100)
         test_metrics = test(model,test_inputs, test_labels, 1)
         print('Testing Performance:', test_metrics)
+            
     else:
-        if(paper_model):
-            for i in range(len(demo_sentences)):
-                prbs = model.call(demo_sentences[i:i+1])
-                guesses = tf.math.argmax(prbs, axis=1).numpy()
-                print('Sentence:', demo_sentences[i])
-                print('True Label:', demo_labels[i])
-                print('Predicted Label:', guesses[0])
-                print('----------')
+        for i in range(len(demo_sentences)):
+            prbs = model.call(demo_sentences[i:i+1])
+            guesses = tf.math.argmax(prbs, axis=1).numpy()
+            print('Sentence:', demo_sentences[i])
+            print('True Label:', demo_labels[i])
+            print('Predicted Label:', guesses[0])
+            print('----------')
 
--- main_lstm.py
+++ main_lstm.py
@@ -1,8 +1,6 @@
-import os
 import preprocessing as preprocess
 import tensorflow as tf
 import LSTM
-from tqdm import tqdm
 
 def run():
     pre = preprocess.parse()
@@ -16,34 +14,38 @@ def run():
     return accuracy
 
 def train(model, train_input, train_labels):
-    batches = int(len(train_input) / model.batch_size)
-    b_size = model.batch_size * model.window_size
+    """
+    Runs through all training examples.
+    :param model: the initilized model to use for forward and backward pass
+    :param train_inputs: train inputs of shape (num_inputs,)
+    :param train_labels: train labels of shape (num_labels,3)
+    :return: Average loss
+    """
     total_loss = 0.0
     total_correct = 0.0
     total = 0.0
     total_square_error = 0.0
-    progress = tqdm(range(batches))
-    progress.set_description('Training ' + str(type(model).__name__))
-    for batch in progress:
+    batches = int(len(train_input) / model.batch_size)
+
+    for batch in range(batches):
         with tf.GradientTape() as tape:
-            inputs = train_input[batch*model.batch_size:(batch+1)*model.batch_size]
+            inputs = train_input[ model.batch_size * batch : model.batch_size * (batch+1)]
             #print('shape',npinputs.shape)
             prbs = model.call(tf.convert_to_tensor(inputs), initial_state=None)
-            labels = train_labels[batch*model.batch_size:(batch+1)*model.batch_size]
-
+            labels = train_labels[model.batch_size * batch : model.batch_size * (batch+1)]
             loss = model.loss(prbs, labels)
             total_loss += loss
+
         guesses = tf.math.argmax(prbs, axis=1).numpy()
         for i in range(model.batch_size):
             print('batch',batch)
             #print('sentence',real_sentences[int(total)])
             #print('logits',prbs[i])
             print('guesses',guesses[i])
-            print('label',real_labels[int(total)])
             total += 1
             total_square_error += (guesses[i] - labels[i]) ** 2
             print('labels[i]',labels[i])
-            if guesses[i]==labels[i]:
+            if labels[i]==guesses[i]:
                 total_correct += 1
                 print('CORRECT!')
             else:
@@ -56,44 +58,43 @@ def train(model, train_input, train_labels):
 
     return total_loss / batches
 
-def test(model, test_input, test_labels):
+def test(model, test_inputs, test_labels):
+    """
+    Runs through all testing examples
+    :param model: the LSTM model that will be used for testing 
+    :param test_inputs: all tokenized sentences for testing
+    :param test_labels: all labels of sentenes for testing
+    :returns: accuracy of model predictions
+    """
+
     pre = preprocess.parse()
     real_sentences = pre[1][0][0]
     real_labels = pre[1][0][1]
-    """
-    Runs through one epoch - all testing examples
-    :param model: the trained model to use for prediction
-    :param test_inputs: train inputs (all inputs for testing) of shape (num_inputs,)
-    :param test_labels: train labels (all labels for testing) of shape (num_labels,)
-    :returns: accuracy of model predictions
-    """
-    batches = int(len(test_input) / model.batch_size)
+
     total_correct = 0.0
     total = 0.0
-    total_square_error = 0.0
+    batches = int(len(test_inputs) / model.batch_size)
 
     for batch in range(batches):
-        inputs = test_input[batch*model.batch_size:(batch+1)*model.batch_size]
+        inputs = test_inputs[batch*model.batch_size:(batch+1)*model.batch_size]
         prbs = model.call(tf.convert_to_tensor(inputs), initial_state=None)
         guesses = tf.math.argmax(prbs, axis=1).numpy()
 
         labels = test_labels[batch*model.batch_size:(batch+1)*model.batch_size]
-
 
         for i in range(model.batch_size):
             print('sentence',real_sentences[int(total)])
             print('logits',prbs[i])
             print('label',real_labels[int(total)])
             total += 1
-            total_square_error += (guesses[i] - labels[i]) ** 2
-            if guesses[i]==labels[i]:
+            if labels[i]==guesses[i]:
                 total_correct += 1
                 print('CORRECT')
             else:
                 print('WRONG')
     print('accuracy',total_correct / total)
     print('total example',total)
-    return total_correct / total, total_square_error / total
+    return total_correct / total
 
 
 if __name__ == "__main__":
